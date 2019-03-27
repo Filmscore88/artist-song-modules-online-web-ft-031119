77f57b226b294eef347e6bf101c9fac3f8e73f9c
Alberto Morales <cmtmorales24@gmail.com>
2019-03-27 20:57:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
 class Song
-  extend Memorable
-  include Paramable
+  extend Memorable::ClassMethods
+  include Paramable::InstanceMethods
   
   attr_accessor :name
   attr_reader :artist
