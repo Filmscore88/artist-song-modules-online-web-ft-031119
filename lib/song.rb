01c3f6daf3c99a8b492fccdc0a9d2739a08c64d7
Alberto Morales <cmtmorales24@gmail.com>
2019-03-27 21:07:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,10 +10,6 @@ class Song
 
   @@songs = []
 
-  #def initialize
-    #self.class.all << self
-  #end
-
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
   end
@@ -22,19 +18,8 @@ class Song
     @@songs
   end
 
-  #def self.reset_all
-   # self.all.clear
-  #end
-
-  #def self.count
-    #self.all.count
-  #end
-
   def artist=(artist)
     @artist = artist
   end
 
-  #def to_param
-   # name.downcase.gsub(' ', '-')
-  #end
 end
