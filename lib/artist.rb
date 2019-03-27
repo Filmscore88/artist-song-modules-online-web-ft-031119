dd6e3351d8ee120757320f2dbf2929956951dae2
Alberto Morales <cmtmorales24@gmail.com>
2019-03-27 21:06:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,14 +25,6 @@ class Artist
     @@artists
   end
 
- # def self.reset_all
-    #self.all.clear
-  #end
-
- # def self.count
-  #  self.all.count
-  #end
-
   def add_song(song)
     @songs << song
     song.artist = self
@@ -42,7 +34,4 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  #def to_param
-    #name.downcase.gsub(' ', '-')
-  #end
 end
